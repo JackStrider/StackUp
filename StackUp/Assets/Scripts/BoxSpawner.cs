f8d24f4e05ded42aa6995189390ad1d31e0bea2d
JackStrider <rossi.william@gmail.com>
2018-03-06 13:28:02 -0600

    Second commit
    Error with movement/throw/jump script. Submitting to send off to professor.

---
+++ StackUp/Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour {
+    //Got part of this script from Lurony
+    //On youtube. Video here: https://www.youtube.com/watch?v=_Xrw2EEhzI4
+
+    public Transform spawnPoint; 
+    public Rigidbody Prefab;
+
+    // Use this for initialization
+    void OnTriggerEnter ()
+    {
+        Rigidbody RidgidPrefab;
+        RidgidPrefab = Instantiate(Prefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
---
+++ StackUp/Assets/Scripts/PlayerCharecter.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerCharecter : MonoBehaviour {
+
+    #region serialzed fields
+    [SerializeField]
+    private float moveSpeed = 10;
+
+    [SerializeField]
+    private int debugPlayerNumberOverride = 0;
+    #endregion
+
+    #region private felds
+    private Rigidbody rigidbody;
+    private Text playerNumberLabel;
+    private Player controllingPlayer_UseProperty;
+    #endregion
+
+    #region public properties 
+    //Which player controls the characyer?
+    //We will use the Player.PlayerNumber. to
+    //decide which GamePad to look at.
+
+        public Player ControllingPlayer
+    {
+        get { return controllingPlayer_UseProperty; }
+        set
+        {
+            controllingPlayer_UseProperty = value;
+            UpdatePlayerIndexLabel();
+        }
+    }
+    #endregion
+
+    #region private propeties
+    private float HorizontalInput
+    {
+        get { return Input.GetAxis(HorizontalInputName); }
+    }
+
+    private float VerticalInput
+    {
+        get { return Input.GetAxis(VerticalInputName); }
+    }
+    private float FireInput
+    {
+        get { return Input.GetAxis(FireInputName); }
+    }
+    private float JumpInput
+    {
+        get { return Input.GetAxis(JumpInputName); }
+    }
+
+    // You must configure the Unity Input Manager
+    // to have an axis for each control for each supported player.
+    // Begin numbering at 1, as Unity numbers "joysticks" beginning at 1.
+    // "Joystick" means gamepad in real life...
+
+    private string HorizontalInputName
+    {
+        get
+        {
+            return "Horizontal" + ControllingPlayer.PlayerNumber;
+        }
+    }
+
+    private string VerticalInputName
+    {
+        get
+        {
+            return "Vertical" + ControllingPlayer.PlayerNumber;
+        }
+    }
+
+    private string FireInputName
+    {
+        get
+        {
+            return "Fire" + ControllingPlayer.PlayerNumber;
+        }
+    }
+
+    private string JumpInputName
+    {
+        get
+        {
+            return "Jump" + ControllingPlayer.PlayerNumber;
+        }
+    }
+    #endregion
+
+    private void UpdatePlayerIndexLabel()
+    {
+        playerNumberLabel.text = ControllingPlayer.PlayerNumber.ToString();
+    }
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerNumberLabel = GetComponentInChildren<Text>();
+
+#if UNITY_EDITOR
+        if (debugPlayerNumberOverride > 0)
+        {
+            ControllingPlayer = new Player(debugPlayerNumberOverride);
+        }
+#endif
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var moveDirection = new Vector2(HorizontalInput, VerticalInput);
+        rigidbody.velocity = moveDirection * moveSpeed;
+    }
+}
