a016ae111a5a4997c1bc1127492712807e9ecbc5
JackStrider <rossi.william@gmail.com>
2018-03-21 14:31:17 -0500

    [Working Build] More additions
    To do list:
    *Right stick movement
    *score area working
    *grab/droping blocks
    *join screen

---
+++ Assets/Scripts/SetColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        //switch()
+        //{
+        //    case (ControllingPlayer.PlayerNumber = 1):
+        //Renderer rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Specular");
+        //rend.material.SetColor("_SpecColor", Color.red);
+        //        break;
+        //}
+    }	
+}
