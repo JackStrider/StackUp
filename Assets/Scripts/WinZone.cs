2aeecf01e557f71869780e39949e04f132f5fb2f
JackStrider <rossi.william@gmail.com>
2018-03-26 10:58:25 -0500

    [Workig build] Final commit before due date
    Worked on:
    *Finish line
    *Start menu
    *House Keeping
    
    Bugs:
    *Jump broken
    *Block stays attached
    *Join screen not integrated

---
+++ Assets/Scripts/WinZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinZone : MonoBehaviour {
+    private bool FinishPlane = false;
+       
+    void Start()
+    {
+        FinishPlane = false;
+    }
+
+    private void Update()
+    {
+        if (FinishPlane == true)
+        {
+            SceneManager.LoadScene("Menu 3D", LoadSceneMode.Single);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            FinishPlane = true;
+        }
+    }
+}
